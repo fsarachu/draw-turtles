afcec5b2cfbf6facbcca11e4d0b1e80f1c8e3420
Franco Sarachu <fsarachu@gmail.com>
2016-09-13 18:16:47 -0300

    Customize donatello!

--- mindstorms.py
+++ mindstorms.py
@@ -3,9 +3,12 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("white")
+    window.bgcolor("#50CC99")
 
     donatello = turtle.Turtle()
+    donatello.shape("turtle")
+    donatello.color("#209C69")
+    donatello.pensize(3)
 
     donatello.forward(200)
     donatello.right(90)
