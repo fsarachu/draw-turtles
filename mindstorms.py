74a2e0e115b96ce31821f6f8a8f12f09795c5e66
Franco Sarachu <fsarachu@gmail.com>
2016-09-13 20:48:39 -0300

    Some other art

--- mindstorms.py
+++ mindstorms.py
@@ -7,38 +7,44 @@ def draw_square(some_turtle, side_length):
         some_turtle.right(90)
 
 
+def draw_triangle(some_turtle, side_length):
+    for i in range(0, 3):
+        some_turtle.forward(side_length)
+        some_turtle.right(120)
+
+
 def draw_semicircle(some_turtle, radius):
     some_turtle.circle(radius, 180)
 
 
 def draw_art():
     window = turtle.Screen()
-    window.bgcolor("#003236")
+    window.bgcolor("#FFFFFF")
 
     leonardo = turtle.Turtle()
     leonardo.hideturtle()
-    leonardo.color("#FB6542")
-    leonardo.pensize(1)
+    leonardo.color("#333333")
+    leonardo.pensize(2)
     leonardo.speed(200)
 
     donatello = turtle.Turtle()
     donatello.hideturtle()
-    donatello.color("#6FB989")
+    donatello.color("#333333")
     donatello.pensize(2)
     donatello.speed(200)
 
     raphael = turtle.Turtle()
     raphael.hideturtle()
-    raphael.color("#66A5AD")
-    raphael.pensize(3)
+    raphael.color("#333333")
+    raphael.pensize(2)
     raphael.speed(200)
 
     for i in range(0, 36):
         draw_square(leonardo, 80)
         leonardo.right(10)
-        draw_square(donatello, 120)
+        draw_triangle(donatello, 280)
         donatello.right(10)
-        draw_square(raphael, 160)
+        raphael.circle(120)
         raphael.right(10)
 
     window.exitonclick()
