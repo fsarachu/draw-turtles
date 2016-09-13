5cf9053c3914236bb7046a0e883c3877bfa485d2
Franco Sarachu <fsarachu@gmail.com>
2016-09-13 17:43:26 -0300

    Add window

--- mindstorms.py
+++ mindstorms.py
@@ -2,8 +2,13 @@ import turtle
 
 
 def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("blue")
+
     donatello = turtle.Turtle()
     donatello.forward(100)
+
+    window.exitonclick()
 
 
 draw_square()
