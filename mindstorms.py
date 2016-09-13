ff657419546c1bf66ecb69c43587857c397421af
Franco Sarachu <fsarachu@gmail.com>
2016-09-13 19:49:38 -0300

    Span a second turtle to draw semicircles

--- mindstorms.py
+++ mindstorms.py
@@ -5,27 +5,30 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor("#50CC99")
 
+    # Draw square
     donatello = turtle.Turtle()
     donatello.shape("turtle")
     donatello.color("#209C69")
     donatello.pensize(3)
+    donatello.speed(1.7)
 
-    donatello.forward(200)
-    donatello.right(90)
-    donatello.forward(200)
-    donatello.right(90)
-    donatello.forward(200)
-    donatello.right(90)
-    donatello.forward(200)
-    donatello.right(180)
+    for i in range(0, 4):
+        donatello.forward(200)
+        donatello.right(90)
 
-    donatello.circle(25, 180)
-    donatello.right(180)
-    donatello.circle(25, 180)
-    donatello.right(180)
-    donatello.circle(25, 180)
-    donatello.right(180)
-    donatello.circle(25, 180)
+    # Draw semicircles
+    raphael = turtle.Turtle()
+
+    raphael.shape("turtle")
+    raphael.color("#209C69")
+    raphael.pensize(2)
+    raphael.speed(6)
+
+    raphael.right(90)
+
+    for i in range(0, 4):
+        raphael.circle(25, 180)
+        raphael.right(180)
 
     window.exitonclick()
 
